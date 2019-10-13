08b4f60916bf6606e13a0a0b4c7e99cbfc91ffe4
Walsh Fernandes <walshfernades.320@gmail.com>
2019-10-13 12:45:33 +0530

    some changes

--- app/src/main/java/com/beachcleanerbot/MainActivity.java
+++ app/src/main/java/com/beachcleanerbot/MainActivity.java
@@ -3,12 +3,14 @@ package com.beachcleanerbot;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    WifiHelper wifiHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +18,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://google.com");
 
-        Log.i("TAG", "WIFI: " + WifiHelper.isWifiEnabled(this));
-        WifiHelper.enableWifiHotspot(this);
+        switchOnHotspot();
+    }
+
+    private void switchOnHotspot() {
+        wifiHelper = new WifiHelper(this);
+        wifiHelper.enableWifiHotspot();
     }
 }
--- app/src/main/java/com/beachcleanerbot/WifiHelper.java
+++ app/src/main/java/com/beachcleanerbot/WifiHelper.java
@@ -3,33 +3,77 @@ package com.beachcleanerbot;
 import android.content.Context;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.LocalOnlyHotspotCallback;
+import android.os.Build;
+import android.os.Handler;
+
+import androidx.annotation.RequiresApi;
 
 import java.lang.reflect.Method;
 
 public class WifiHelper {
 
-    public static boolean isWifiEnabled (Context context) {
-        WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        return wifiManager.isWifiEnabled();
+    private Context context;
+    private WifiManager wifiManager;
+    private WifiManager.LocalOnlyHotspotReservation reservation;
+
+    public WifiHelper(Context context) {
+        this.context = context;
+        wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
     }
 
-    public static boolean enableWifiHotspot(Context context) {
-        WifiManager wifimanager = (WifiManager) context.getApplicationContext().getSystemService(context.WIFI_SERVICE);
+    private boolean enableWifiHotspotBelowAPI8() {
         WifiConfiguration wificonfiguration = new WifiConfiguration();
         wificonfiguration.SSID = "walsh";
+        wificonfiguration.preSharedKey = "gitbtitw";
         try {
             // if WiFi is on, turn it off
-            if(isWifiEnabled(context)) {
-                wifimanager.setWifiEnabled(false);
+            if(isWifiOn()) {
+                wifiManager.setWifiEnabled(false);
             }
-            Method method = wifimanager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
-            method.invoke(wifimanager, wificonfiguration, !isWifiEnabled(context));
+            Method method = wifiManager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+            method.invoke(wifiManager, wificonfiguration, !isWifiOn());
             return true;
         }
         catch (Exception e) {
             e.printStackTrace();
         }
         return false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void enableWifiHotspotAboveAPI8() {
+        wifiManager.startLocalOnlyHotspot(new LocalOnlyHotspotCallback(){
+            @Override
+            public void onStarted(WifiManager.LocalOnlyHotspotReservation mReservation) {
+                super.onStarted(mReservation);
+                reservation = mReservation;
+            }
+
+            @Override
+            public void onStopped() {
+                super.onStopped();
+            }
+
+            @Override
+            public void onFailed(int reason) {
+                super.onFailed(reason);
+            }
+        }, new Handler());
+    }
+
+    public void enableWifiHotspot() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // android api version greater than marshmallow
+            enableWifiHotspotAboveAPI8();
+        } else {
+            // android api version lower than marshmallow
+            enableWifiHotspotBelowAPI8();
+        }
+    }
+
+    public boolean isWifiOn() {
+        return wifiManager.isWifiEnabled();
     }
 
 }
